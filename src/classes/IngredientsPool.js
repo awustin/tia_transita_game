25a0e7bc2942872bb7699532bfcf6c30fec2dd47
Agustin García <agustin94garcia@gmail.com>
2024-05-12 14:35:52 +0100

    Added class: IngredientsPool

---
+++ src/classes/IngredientsPool.js
@@ -0,0 +1,136 @@
+/**
+ * `IngredientsPool` controls the available ingredients in the game.
+ * It keeps track of a list of 4 ingredients with the probability
+ * that each one has to come up next.
+ */
+export default class IngredientsPool {
+    _sortedIngredients = [];
+    _sortedProbabilitySegments = [];
+    
+    constructor(initArray = null) {
+        if (!initArray) {
+            throw {
+                message: 'Empty ingredients list',
+                code: 'C01'
+            };
+        } else if (!initArray.length == 4) {
+            throw {
+                message: 'Ingredients list should be 4',
+                code: 'C02'
+            };
+        }
+
+        this._updatePool(initArray);
+    }
+
+    /**
+     * Gets an array of ingredients based on each probability
+     * @param {Number} amount If not specified, it returns a single ingredient
+     * @returns List of ingredients
+     */
+    getNextIngredients(amount = 1) {
+        return new Array(amount)
+            .fill(null)
+            .map(() => this._pickIngredientWithProbability());
+    }
+
+    /**
+     * Adds an ingredient to the pool. It always keeps 4 ingredients.
+     * The ingredient with the higher probability gets discarted.
+     * It recalculates the probabilities to distribute them evenly.
+     * @param {*} newIngredient
+     * @returns Discarded ingredient
+     */
+    addIngredient(newIngredient = {}) {
+        const ingredients = this._sortedIngredients;
+        const mostLikelyIngredient = ingredients.reduce((acc, curr, index) => {
+            if (!index || (curr.probability >= acc?.probability))
+                return curr;
+        }, {});
+        const restockedIngredients = ingredients.filter(({ id }) => Number(id) !== Number(mostLikelyIngredient.id))
+        
+        restockedIngredients.push(newIngredient);
+        
+        this._updatePool(restockedIngredients.map(ingredient => ({ ...ingredient, probability: 1/4 })));
+
+        return mostLikelyIngredient;
+    }
+
+    /**
+     * Redistributes the probability of each ingredient
+     * based on the amount of each type passed. The higher the amount, the lower the probability
+     * @param {Object} ingredientsAmount Object with ids as keys and amounts as values
+     * `{ ... <id>: <amount> }`
+     */
+    redistributeProbabilities(ingredientsAmount = {}) {
+
+        if (Object.keys(ingredientsAmount).length) {
+            const sortedIngredients = this._sortedIngredients;
+            let max = 0;
+            let totalInversed = 0;
+            const inversedAmounts = {};
+
+            // Filter the ids present in the pool
+            const filteredIngredientsIds = Object.keys(ingredientsAmount)
+                .filter(id =>
+                    sortedIngredients.find(ingredient => Number(id) === Number(ingredient.id))
+                );
+
+            // Get the max amount
+            max = Math.max(...filteredIngredientsIds.map(id => ingredientsAmount[id]));
+
+            // Inverse amounts to compute probabilities
+            filteredIngredientsIds.forEach(id => {
+                const amount = ingredientsAmount[id];
+                const inverse = 2 * max - amount;
+
+                inversedAmounts[id] = inverse;
+                totalInversed += inverse;
+            });
+
+            this._updatePool(sortedIngredients.map(ingredient => ({
+                ...ingredient,
+                probability: inversedAmounts[ingredient.id] / totalInversed,
+            })));
+        }
+    }
+
+    // Private functions
+
+    _updatePool(list) {
+        this._sortedIngredients = this._setSortedIngredients(list);
+        this._sortedProbabilitySegments = this._updateProbabilitySegments();
+    }
+
+    _setSortedIngredients(list) {
+        return list.sort((a, b) => Number(a.probability) - Number(b.probability));
+    }
+
+    _updateProbabilitySegments() {
+        const sortedIngredients = this._sortedIngredients;
+
+        // Accumulated probabilities: [0.34, 0.33, 0.33] => [0.34, 0.67, 1]
+        return sortedIngredients.reduce((acc, curr, index) => {
+            const { id, probability } = curr;
+
+            if (!index) {
+                acc.push({ id, probability });
+            } else {
+                const lastProbability = acc[index - 1].probability;
+
+                acc.push({ id, probability: lastProbability + probability })
+            }
+
+            return acc;
+        }, []);
+    }
+
+    _pickIngredientWithProbability() {
+        const ingredients = this._sortedIngredients;
+        const sortedProbabilitySegments = this._sortedProbabilitySegments;
+        const target = Math.random();
+        const picked = sortedProbabilitySegments.find(upperBound => target <= upperBound.probability);
+
+        return ingredients.find(({ id }) => Number(picked.id) === Number(id));
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,46 +1,51 @@
+import IngredientsPool from './classes/IngredientsPool';
 
-class Example extends Phaser.Scene
-{
-    preload ()
+try {
+    class Example extends Phaser.Scene
     {
-        this.load.setBaseURL('https://labs.phaser.io');
-
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
-    }
-
-    create ()
-    {
-        this.add.image(400, 300, 'sky');
-
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
-
-        const logo = this.physics.add.image(400, 100, 'logo');
-
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-
-        particles.startFollow(logo);
-    }
-}
-
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: Example,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
+        preload ()
+        {
+            this.load.setBaseURL('https://labs.phaser.io');
+    
+            this.load.image('sky', 'assets/skies/space3.png');
+            this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+            this.load.image('red', 'assets/particles/red.png');
+        }
+    
+        create ()
+        {
+            this.add.image(400, 300, 'sky');
+    
+            const particles = this.add.particles(0, 0, 'red', {
+                speed: 100,
+                scale: { start: 1, end: 0 },
+                blendMode: 'ADD'
+            });
+    
+            const logo = this.physics.add.image(400, 100, 'logo');
+    
+            logo.setVelocity(100, 200);
+            logo.setBounce(1, 1);
+            logo.setCollideWorldBounds(true);
+    
+            particles.startFollow(logo);
         }
     }
-};
-
-const game = new Phaser.Game(config);
+    
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        scene: Example,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 200 }
+            }
+        }
+    };
+    
+    const game = new Phaser.Game(config);
+} catch (e) {
+    throw new Error(e);
+}
